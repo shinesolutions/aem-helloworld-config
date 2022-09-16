b6d1380c5c7b9fa689d6fc3e482ae9cfeff4ffb8
PradKhandelwal <prad.khandelwal@shinesolutions.com>
2022-09-16 11:05:42 +1000

    Adding profiles.

--- test-aoc/seed-aoc-test-pipelines/seedJob.groovy
+++ test-aoc/seed-aoc-test-pipelines/seedJob.groovy
@@ -13,3 +13,51 @@ pipelineJob('test-aem-opencloud/test-nb-profile') {
         }
     }
 }
+
+pipelineJob('test-aem-opencloud/test-ausp-profile') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('shinesolutions/aem-helloworld-config')
+                    }
+                    branch('adding-end-to-end-testing-profile')
+                }
+            }
+            scriptPath('test-aoc/test-ausp-profile/Jenkinsfile')
+        }
+    }
+}
+
+pipelineJob('test-aem-opencloud/test-shineworks-amazon-linux2-profile') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('shinesolutions/aem-helloworld-config')
+                    }
+                    branch('adding-end-to-end-testing-profile')
+                }
+            }
+            scriptPath('test-aoc/test-amazon-linux2-profile/Jenkinsfile')
+        }
+    }
+}
+
+pipelineJob('test-aem-opencloud/test-shineworks-rhel7-profile') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('shinesolutions/aem-helloworld-config')
+                    }
+                    branch('adding-end-to-end-testing-profile')
+                }
+            }
+            scriptPath('test-aoc/test-rhel7-profile/Jenkinsfile')
+        }
+    }
+}
