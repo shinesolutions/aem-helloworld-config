ad364d08fbdb8bf1d6563ce457877671a101d574
PradKhandelwal <prad.khandelwal@shinesolutions.com>
2022-10-19 14:40:13 +1100

    Modifying seed job pipeline.

--- test-aoc/seed-aoc-test-pipelines/seedJob.groovy
+++ test-aoc/seed-aoc-test-pipelines/seedJob.groovy
@@ -1,10 +1,6 @@
 pipelineJob('test-aem-opencloud/test-nb-profile') {
     parameters {
-        string{
-            name('AOC_CONFIG_PROFILE_STACK_BUILDER')
-            defaultValue('aem-rhel7-aem65-jdk11-ap')
-            description('Name of the stack-builder configuration profile.')
-        }
+        stringParam('AOC_CONFIG_PROFILE_STACK_BUILDER','aem-rhel7-aem65-jdk11-ap','Name of the stack-builder configuration profile.')
     }
     definition {
         cpsScm {
@@ -24,6 +20,7 @@ pipelineJob('test-aem-opencloud/test-nb-profile') {
 
 pipelineJob('test-aem-opencloud/test-ap-profile') {
     parameters {
+        stringParam('AOC_CONFIG_PROFILE_STACK_BUILDER', 'aem-rhel7-aem65-jdk8-nb', 'Name of the stack-builder configuration profile.')
     }
     definition {
         cpsScm {
@@ -43,6 +40,7 @@ pipelineJob('test-aem-opencloud/test-ap-profile') {
 
 pipelineJob('test-aem-opencloud/test-sh1-rhel7-profile') {
     parameters {
+        stringParam('AOC_CONFIG_PROFILE_STACK_BUILDER', 'aem-rhel7-aem65-jdk11-sh', 'Name of the stack-builder configuration profile.')
     }
     definition {
         cpsScm {
@@ -62,6 +60,7 @@ pipelineJob('test-aem-opencloud/test-sh1-rhel7-profile') {
 
 pipelineJob('test-aem-opencloud/test-sh2-amazon-linux2-profile') {
     parameters {
+        stringParam('AOC_CONFIG_PROFILE_STACK_BUILDER', 'aem-amazon-linux2-aem65-jdk11-sh', 'Name of the stack-builder configuration profile.')
     }
     definition {
         cpsScm {
