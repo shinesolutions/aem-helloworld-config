b9d0b303a868bda5667a05dadd1b9177602764ac
PradKhandelwal <prad.khandelwal@shinesolutions.com>
2022-11-08 16:30:58 +1100

    Added trigger in the seed job script for the pipeline.

--- test-aoc/seed-aoc-test-pipelines/seedJob.groovy
+++ test-aoc/seed-aoc-test-pipelines/seedJob.groovy
@@ -1,24 +1,7 @@
-pipelineJob('test-aem-opencloud/test-nb-profile') {
-    parameters {
-        stringParam('AOC_CONFIG_PROFILE_STACK_BUILDER','aem-rhel7-aem65-jdk8-nb ','Name of the stack-builder configuration profile.')
-    }
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        github('shinesolutions/aem-helloworld-config')
-                        credentials('pradkhandelwal')
-                    }
-                  branch('adding-end-to-end-testing-profile')
-                }
-            }
-            scriptPath('test-aoc/test-nb-profile/Jenkinsfile')
-        }
-    }
-}
-
 pipelineJob('test-aem-opencloud/test-ap-profile') {
+    triggers {
+        cron("H 6 * * 1-5")
+    }
     parameters {
         stringParam('AOC_CONFIG_PROFILE_STACK_BUILDER', 'aem-rhel7-aem65-jdk11-ap', 'Name of the stack-builder configuration profile.')
     }
@@ -38,7 +21,33 @@ pipelineJob('test-aem-opencloud/test-ap-profile') {
     }
 }
 
+pipelineJob('test-aem-opencloud/test-nb-profile') {
+    triggers {
+        cron("H 9 * * 1-5")
+    }
+    parameters {
+        stringParam('AOC_CONFIG_PROFILE_STACK_BUILDER','aem-rhel7-aem65-jdk8-nb ','Name of the stack-builder configuration profile.')
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('shinesolutions/aem-helloworld-config')
+                        credentials('pradkhandelwal')
+                    }
+                  branch('adding-end-to-end-testing-profile')
+                }
+            }
+            scriptPath('test-aoc/test-nb-profile/Jenkinsfile')
+        }
+    }
+}
+
 pipelineJob('test-aem-opencloud/test-sh1-rhel7-profile') {
+    triggers {
+        cron("H 12 * * 1-5")
+    }
     parameters {
         stringParam('AOC_CONFIG_PROFILE_STACK_BUILDER', 'aem-rhel7-aem65-jdk11-sh', 'Name of the stack-builder configuration profile.')
     }
@@ -59,6 +68,9 @@ pipelineJob('test-aem-opencloud/test-sh1-rhel7-profile') {
 }
 
 pipelineJob('test-aem-opencloud/test-sh2-amazon-linux2-profile') {
+    triggers {
+        cron("H 15 * * 1-5")
+    }
     parameters {
         stringParam('AOC_CONFIG_PROFILE_STACK_BUILDER', 'aem-amazon-linux2-aem65-jdk11-sh', 'Name of the stack-builder configuration profile.')
     }
